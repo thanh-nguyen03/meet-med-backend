bd5078930a9d1bff2df9de7341971e2077938a64
thanhnguyenduc <nguyenducthanh240903@gmail.com>
2024-09-01 21:06:16 +0700

    Init commit

---
+++ src/test/java/com/thanhnd/clinic_application/ClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanhnd.clinic_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
