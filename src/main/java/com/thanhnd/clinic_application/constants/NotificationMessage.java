8049a98a6f842c57fce3591884dcb5ba535ec443
Nguyen Duc Thanh <nguyenducthanh240903@gmail.com>
2024-11-30 01:45:02 +0700

    add: setup cron job to send working shift reminder notification

--- src/main/java/com/thanhnd/clinic_application/constants/NotificationMessage.java
+++ src/main/java/com/thanhnd/clinic_application/constants/NotificationMessage.java
@@ -7,6 +7,9 @@ public enum NotificationMessage {
 	APPOINTMENT_REMINDER_TITLE("Appointment Reminder"),
 	APPOINTMENT_REMINDER_24_HOUR_MESSAGE("You have an appointment with Doctor %s tomorrow at %s. Please check the detail information."),
 	APPOINTMENT_REMINDER_1_HOUR_MESSAGE("You have an appointment with Doctor %s in 1 hour. Please check the detail information."),
+
+	WORKING_SHIFT_REMINDER_TITLE("Working Shift Reminder"),
+	WORKING_SHIFT_REMINDER_TOMORROW_MESSAGE("You have a working shift tomorrow. Please check the detail information."),
 	;
 
 	private final String message;
