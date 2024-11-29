912cf4ff8e04315babcdb08946678927ca80672f
Nguyen Duc Thanh <nguyenducthanh240903@gmail.com>
2024-11-29 23:38:25 +0700

    update: send notification using fcm

---
+++ src/main/java/com/thanhnd/clinic_application/modules/fcm_device_token/repository/FcmDeviceTokenRepository.java
@@ -0,0 +1,15 @@
+package com.thanhnd.clinic_application.modules.fcm_device_token.repository;
+
+import com.thanhnd.clinic_application.common.repository.BaseRepository;
+import com.thanhnd.clinic_application.entity.FcmDeviceToken;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface FcmDeviceTokenRepository extends BaseRepository<FcmDeviceToken, String> {
+	Optional<FcmDeviceToken> findByToken(String token);
+
+	List<FcmDeviceToken> findAllByUserId(String userId);
+}
