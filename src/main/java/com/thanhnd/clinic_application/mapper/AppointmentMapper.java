4011f36742734da45831cb7417440e1956f5087c
Nguyen Duc Thanh <nguyenducthanh240903@gmail.com>
2024-11-30 11:40:25 +0700

    fix: mapping doctor for appointment dto

--- src/main/java/com/thanhnd/clinic_application/mapper/AppointmentMapper.java
+++ src/main/java/com/thanhnd/clinic_application/mapper/AppointmentMapper.java
@@ -1,7 +1,9 @@
 package com.thanhnd.clinic_application.mapper;
 
 import com.thanhnd.clinic_application.entity.Appointment;
+import com.thanhnd.clinic_application.entity.Department;
 import com.thanhnd.clinic_application.modules.appointments.dto.AppointmentDto;
+import com.thanhnd.clinic_application.modules.departments.dto.DepartmentDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -9,7 +11,9 @@ import org.mapstruct.Mapping;
 public interface AppointmentMapper extends IBaseMapper<Appointment, AppointmentDto> {
 	@Override
 	@Mapping(target = "doctor", source = "registeredShiftTimeSlot.registeredShift.doctor")
-	@Mapping(target = "doctor.department.doctors", ignore = true)
-	@Mapping(target = "doctor.department.headDoctor", ignore = true)
 	AppointmentDto toDto(Appointment entity);
+
+	@Mapping(target = "doctors", ignore = true)
+	@Mapping(target = "headDoctor", ignore = true)
+	DepartmentDto toDepartmentDto(Department department);
 }
--- src/main/java/com/thanhnd/clinic_application/modules/notifications/repository/NotificationRepository.java
+++ src/main/java/com/thanhnd/clinic_application/modules/notifications/repository/NotificationRepository.java
@@ -3,6 +3,8 @@ package com.thanhnd.clinic_application.modules.notifications.repository;
 import com.thanhnd.clinic_application.common.repository.BaseRepository;
 import com.thanhnd.clinic_application.constants.NotificationStatus;
 import com.thanhnd.clinic_application.entity.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +12,8 @@ import java.util.Optional;
 
 @Repository
 public interface NotificationRepository extends BaseRepository<Notification, String> {
+	Page<Notification> findAllByReceiverId(String receiverId, Pageable pageable);
+
 	Optional<Notification> findByIdAndReceiverId(String id, String receiverId);
 
 	List<Notification> findAllByReceiverIdAndStatusEquals(String receiverId, NotificationStatus status);
