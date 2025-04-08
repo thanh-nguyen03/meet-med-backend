c0688bb815e93d42b030520db394686058de2634
linhlk11 <linhmit03@gmail.com>
2025-04-08 23:52:36 +0700

    push code 1

---
+++ src/test/java/com/thanhnd/clinic_application/modules/department/DepartmentServiceIntegrationTest.java
@@ -0,0 +1,4 @@
+package com.thanhnd.clinic_application.modules.department;
+
+public class DepartmentServiceIntegrationTest {
+}
---
+++ src/test/java/com/thanhnd/clinic_application/modules/symtom/SymptomServiceImplTest.java
@@ -0,0 +1,4 @@
+package com.thanhnd.clinic_application.modules.symtom;
+
+public class SymptomServiceImplTest {
+}
